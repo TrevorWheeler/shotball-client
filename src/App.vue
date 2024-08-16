dcf0fdcb2b11530d435a3207c28564eb4cd9fc68
Trevor Wheeler <sendtrevmail@gmail.com>
2024-08-17 00:47:18 +1000

    remove unwanted script

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@ import { useStore } from '@/store';
 import { onMounted } from 'vue';
 const store = useStore();
 onMounted(async () => {
-  // let XWigetscript = document.createElement('script')
-  // XWigetscript.setAttribute('src', 'https://platform.twitter.com/widgets.js')
-  // document.head.appendChild(XWigetscript)
 
-  // loadScript('https://platform.twitter.com/widgets.js').then(() => {
-  //   console.log("LOADED")
-  // })
 });
 
 
